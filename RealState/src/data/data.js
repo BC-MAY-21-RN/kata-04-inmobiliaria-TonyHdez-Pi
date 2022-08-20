e8418c82618c4e18eb2c88358a32ed9253bbe706
Antonio de Jesus Hernandez Piña <tonyhdezpi@gmail.com>
2022-08-19 23:51:32 -0500

    Create data file
    -Create data file wich is a JSON file

---
+++ RealState/src/data/data.js
@@ -0,0 +1,123 @@
+export const data =[
+    {
+      "id": 1,
+    "image": "https://amazingarchitecture.com/storage/171/84999790_image9405.jpg",
+    "propertyName": "Entreparotas House",
+    "address": "Residecial las parotas. Colima, colima. Mexico",
+    "bedrooms": 3,
+    "bathrooms": 3,
+    "squareFeet": 350,
+    "price": 1000
+    },
+    {
+      "id": 2,
+    "image": "https://amazingarchitecture.com/storage/1018/casa-mao-di-frenna-arquitectos-mexico.jpg",
+    "propertyName": "Mao House",
+    "address": "Colima city, Colima, Mexico.",
+    "bedrooms": 4,
+    "bathrooms": 2,
+    "squareFeet": 450,
+    "price": 850
+    },
+    {
+      "id": 3,
+    "image": "https://assets.easybroker.com/property_images/634193/9805257/EB-BK4193.jpg?version=1517419312",
+    "propertyName": "Brand new house",
+    "address": " Alfredo V Bonfil, Villa de Alvarez, Colima",
+    "bedrooms": 3,
+    "bathrooms": 3,
+    "squareFeet": 200,
+    "price": 1500
+    },
+    {
+      "id": 4,
+    "image": "https://assets.easybroker.com/property_images/634181/9756788/EB-BK4181.jpg?version=1516982506",
+    "propertyName": "Pistachio",
+    "address": " Real Santa Bárbara, Colima, Colima",
+    "bedrooms": 2,
+    "bathrooms": 1,
+    "squareFeet": 100,
+    "price": 100
+    },
+    {
+      "id": 5,
+    "image": "https://assets.easybroker.com/property_images/634151/9832319/EB-BK4151.jpg?version=1517595267",
+    "propertyName": "House near sam's club",
+    "address": " Jardines Vista Hermosa, Colima, Colima",
+    "bedrooms": 3,
+    "bathrooms": 2,
+    "squareFeet": 500,
+    "price": 900
+    },
+    {
+      "id": 6,
+    "image": "https://assets.easybroker.com/property_images/634294/14302429/EB-BK4294.jpg?version=1550087742",
+    "propertyName": "Pretty house at nort of colima",
+    "address": " Las Lagunas, Villa de Alvarez, Colima",
+    "bedrooms": 3,
+    "bathrooms": 2,
+    "squareFeet": 360,
+    "price": 450
+    },
+    {
+      "id": 7,
+    "image": "https://assets.easybroker.com/property_images/642211/9833033/EB-BL2211.JPG?version=1517596768",
+    "propertyName": "Esmeralda house",
+    "address": " Esmeralda, Colima, Colima",
+    "bedrooms": 4,
+    "bathrooms": 3,
+    "squareFeet": 400,
+    "price": 650
+    },
+    {
+      "id": 8,
+    "image": "",
+    "propertyName": "Entreparotas House",
+    "address": "Residecial las parotas. Colima, colima. Mexico",
+    "bedrooms": 3,
+    "bathrooms": 3,
+    "squareFeet": 350,
+    "price": 1000
+    },
+    {
+      "id": 9,
+    "image": "https://static-mx.lamudi.com/static/media/bm9uZS9ub25l/2x2x5x880x450/99c6e894c89d54.webp",
+    "propertyName": "Pearl house",
+    "address": " Residencial Primaveras Colima ",
+    "bedrooms": 3,
+    "bathrooms": 2,
+    "squareFeet": 550,
+    "price": 780
+    },
+    {
+      "id": 10,
+    "image": "https://img.vivanuncios.com.mx/api/v1/mx-pt10-ads/images/31/319f2e82-6bfc-424e-8cd2-f9283b00d8f3?rule=s-I3.auto",
+    "propertyName": "Entreparotas House",
+    "address": "Lomas Verdes. Colima, colima. Mexico",
+    "bedrooms": 3,
+    "bathrooms": 3,
+    "squareFeet": 450,
+    "price": 1200
+    },
+    {
+      "id": 11,
+    "image": "https://images.rentals.ca/property-pictures/medium/vancouver-bc/481304/house-4964195.jpg",
+    "propertyName": "Apartment",
+    "address": "151 15th Street East - North Vancouver, BC ",
+    "bedrooms": 8,
+    "bathrooms": 8,
+    "squareFeet": 546,
+    "price": 12000
+    },
+    {
+      "id": 12,
+    "image": "https://images.rentals.ca/property-pictures/medium/north-vancouver-bc/293928/apartment-2595817.jpg",
+    "propertyName": "Fifth floor apartment",
+    "address": "150 East Keith Road - North Vancouver, BC ",
+    "bedrooms": 1,
+    "bathrooms": 1,
+    "squareFeet": 864,
+    "price": 3756
+    }
+  ]
+    
